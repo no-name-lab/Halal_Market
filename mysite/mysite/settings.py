261d7c45e76758b64e8d76215a60d8d80a89ed50
asema <shaiycbekovaasemaasema@gmail.com>
2025-08-02 18:35:07 +0600

    cors added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,9 +48,12 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_rest_passwordreset',
     'django_filters',
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -163,3 +166,25 @@ EMAIL_HOST_PASSWORD = 'mprs vrtd xcie nkdz'
 
 AUTH_USER_MODEL = 'users.UserProfile'
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-requested-with",
+]
